7247a2959343cb79de0be9f36aee53f10f475b90
voyagecnft <voyagecnft@gmail.com>
2022-08-04 23:12:04 +0530

    adding files through upload

--- generateResources.py
+++ generateResources.py
@@ -1,4 +1,24 @@
-import os
+import os,json
 
-w=os.listdir('planetMetadata')
-print(len(w))
+
+# 4 tables, one for each resource
+# if tables absent, add tables
+
+# flow 
+# get assets, hashmap for stake key to planets, continents and floating continents -
+# iterate over all assets 
+# if planet - check for multiplier 
+
+def getSisters(planet):
+    with open(f'planetMetadata/{planet}.metadata','r') as f:
+       
+        data=json.load(f)
+        f.close()
+        return data['721']['<policy_id>'][planet]['sisterPlanets']
+
+
+
+
+
+if __name__=="__main__":
+    print(getSisters('Aquapolis55'))
